4372338fcc1d16827777ee0ca6380a2b36f7cb35
Mark Sanders <mark.sanders@cyberfortress.com>
2020-03-04 21:38:39 -0600

    Add events when http request fails

--- src/install.js
+++ src/install.js
@@ -1,33 +1,49 @@
 /* @flow */
+import axios from 'axios';
 export let _Vue;
 
-export function install (Vue, options = {}) {
-    const customProperty = options.customProperty || '$instance';
+let v;
+
+export function install (Vue) {
+
     // Used to avoid multiple mixins being setup
     // when in dev mode and hot module reload
     // https://github.com/vuejs/vue/issues/5089#issuecomment-284260111
-    if (install.installed && _Vue === Vue) return;
-    install.installed = true;
+    if (install.axios_installed && v === _Vue) return;
+    install.axios_installed = true;
 
     Vue.mixin({
-        beforeCreate(): void {
-            // Bind to root VM
-            if (this.$options.axiosJwtHandler !== undefined) {
-                this.$options.axiosJwtHandler.init(Vue);
-                this._axiosJwtHandlerRoot = this;
-                this._axiosJwtHandler = this.$options.axiosJwtHandler;
-                this._axiosJwtHandler.init(this);
+        beforeCreate() {
+            // Bind to root component
+            if (this.$options.axiosJwtHandler) {
+                this._axiosHandlerRoot = this;
+                this._axiosJwtHandler = this.$options.axiosJwtHandler.init(this);
             } else {
-                // Bind child to root vm's axios jwt handler.
-                this._axiosJwtHandlerRoot = (this.$parent && this.$parent._axiosJwtHandlerRoot)
+                this._axiosHandlerRoot = (this.$parent && this.$parent._axiosHandlerRoot) || this
             }
+
+            axios.interceptors.response.use(response => {
+                return response;
+            }, e => {
+                // emit to parent
+                if (400 <= e.response.status < 500) {
+                    this.$emit('http-error', e);
+                    return Promise.reject(e);
+                }
+                // Emit to main app event bus.
+                this.$root.$emit('http-error', e);
+                return Promise.reject(e);
+            });
         }
     });
 
-    Object.defineProperty(Vue.prototype, customProperty, {
-        get() { return this._axiosJwtHandlerRoot._axiosJwtHandler.instance; }
-    });
     Object.defineProperty(Vue.prototype, '$axiosJwtHandler', {
-        get() { return this._axiosJwtHandlerRoot._axiosJwtHandler; }
+        get() { return this._axiosHandlerRoot._axiosJwtHandler; }
     });
+
+    Object.defineProperty(Vue.prototype, '$axios', {
+        get() { return this._axiosHandlerRoot._axiosJwtHandler.instance; }
+    });
+    v = _Vue;
 }
+
